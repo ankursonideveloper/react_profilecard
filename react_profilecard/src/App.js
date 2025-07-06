53963571c7f42e60c794de77a9a488a3a1fd38a0
Ankur Soni <ankur@sciometrix.com>
2025-07-07 01:31:10 +0530

    started

---
+++ react_profilecard/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className = "text-yellow-950">Hi this is the first paragraph</p>
+      <p className = "text-red-950">Hi this is the first paragraph</p>
+      <p className='text-blue-500 underline'>Hi this is the first paragraph</p>
+    </div>
+  );
+}
+
+export default App;
